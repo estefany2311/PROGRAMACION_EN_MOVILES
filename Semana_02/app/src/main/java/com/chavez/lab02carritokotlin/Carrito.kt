44a557ffab1977fe39e1d82a0588f772b2847867
Karla <karla57cl@gmail.com>
2026-08-27 01:46:02 -0500

    Agrega data class Producto y lista de productos

--- Semana_02/app/src/main/java/com/chavez/lab02carritokotlin/Carrito.kt
+++ Semana_02/app/src/main/java/com/chavez/lab02carritokotlin/Carrito.kt
@@ -1,7 +1,28 @@
 package com.chavez.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
     println("=========================================")
+
+    val nombreCliente = "Karla Chavez"
+    val carrito = mutableListOf<Producto>()
+
+    println("Cliente: $nombreCliente\n")
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Funda protectora", 120.0, 1))
+    carrito.add(Producto("Audifonos Pro3", 25.0, 3))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+
 }
