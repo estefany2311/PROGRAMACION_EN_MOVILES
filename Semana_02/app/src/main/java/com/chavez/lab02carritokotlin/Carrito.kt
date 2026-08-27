c117838f93a31e97bbebae2f0cae13514532569d
Karla <karla57cl@gmail.com>
2026-08-27 02:13:03 -0500

    Agraga funciones de subtotal, IGV y total

--- Semana_02/app/src/main/java/com/chavez/lab02carritokotlin/Carrito.kt
+++ Semana_02/app/src/main/java/com/chavez/lab02carritokotlin/Carrito.kt
@@ -21,8 +21,35 @@ fun main() {
     carrito.add(Producto("Funda protectora", 120.0, 1))
     carrito.add(Producto("Audifonos Pro3", 25.0, 3))
 
+
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
 
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+
+    println()
+    println(String.format("Subtotal : S/ %.2f", subtotal))
+    println(String.format("IGV (18%%): S/ %.2f", igv))
+    println(String.format("TOTAL    : S/ %.2f", total))
+}
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    return subtotal * 0.18
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
 }
