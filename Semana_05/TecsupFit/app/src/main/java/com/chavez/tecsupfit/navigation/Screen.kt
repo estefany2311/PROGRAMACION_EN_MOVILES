dec6137d54599440afe1cdfa8772cedd37c41b75
estefany2311 <karla.chavez@tecsup.edu.pe>
2026-09-23 10:32:09 -0500

    Configurar sealed class Screen con rutas de navegacion

--- Semana_05/TecsupFit/app/src/main/java/com/chavez/tecsupfit/navigation/Screen.kt
+++ Semana_05/TecsupFit/app/src/main/java/com/chavez/tecsupfit/navigation/Screen.kt
@@ -1,4 +1,14 @@
 package com.chavez.tecsupfit.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object ClassDetail : Screen("class_detail/{claseId}") {
+        fun createRoute(claseId: Int) = "class_detail/$claseId"
+    }
+    object Confirmation : Screen("confirmation/{claseId}") {
+        fun createRoute(claseId: Int) = "confirmation/$claseId"
+    }
+    object Reservations : Screen("reservations")
+    object Routines : Screen("routines")
+    object Profile : Screen("profile")
 }
