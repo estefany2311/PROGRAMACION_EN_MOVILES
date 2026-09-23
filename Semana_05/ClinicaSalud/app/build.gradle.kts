8c65223dd372c5c6b116f084fc1211d65fccdb0f
estefany2311 <karla.chavez@tecsup.edu.pe>
2026-09-23 04:10:49 -0500

    Agregar dependencias de navegacion e iconos de Material 3

--- Semana_05/ClinicaSalud/app/build.gradle.kts
+++ Semana_05/ClinicaSalud/app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    // Navegación en Jetpack Compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.3")
 }
